81280b7d7c7f7f9b70ff160c4f279cbc668f7f3b
Naoumi1214 <wada121400@gmail.com>
2020-03-29 00:00:15 +0900

    usersテーブルのiconカラムの位置を変更

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -44,5 +44,39 @@ class UsersController extends Controller
         return response()->json($param);
     }
 
+    /**
+     * ユーザー情報の更新
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request)
+    {
+        # code...
+        # code...
+        $param = [
+            'createResult' => true,
+        ];
 
+        //バリデーションの検証
+        $validationResult =  User::validator($request->all());
+
+        //バリデーションの結果が駄目か？
+        if ($validationResult->fails()) {
+            # code...
+            $param['createResult'] = false;
+            $param['error'] = $validationResult->messages();
+            return response()->json($param);
+        }
+
+        //ユーザー登録
+        $updateparam = [
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'icon' => 'dentaku_syoumen_small.png',
+        ];
+        User::create($updateparam);
+
+        return response()->json($param);
+    }
 }
--- database/migrations/2020_03_28_100317_add_column_users_table.php
+++ database/migrations/2020_03_28_100317_add_column_users_table.php
@@ -15,7 +15,6 @@ class AddColumnUsersTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
-            $table->string('icon');
         });
     }
 
