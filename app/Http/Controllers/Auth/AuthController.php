f9d1ee157965839b2a3605b3c8e0bc3c21948b79
Naoumi1214 <wada121400@gmail.com>
2020-04-05 18:52:54 +0900

    ログイン失敗のレスポンスステータスを200に変更

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -30,7 +30,7 @@ class AuthController extends Controller
         $credentials = request(['email', 'password']);
 
         if (!$token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => true]);
         }
 
         return $this->respondWithToken($token);
