df0f9143c0315678820ad36b36d2837509af69a2
Naoumi1214 <wada121400@gmail.com>
2020-04-11 19:00:51 +0900

    勘定項目のtagsテーブルのマイグレーションファイルの実装が完了した。 また、そのテーブルに対応するモデルとコントローラーを生成した。

---
+++ database/migrations/2020_04_11_093601_create_tags_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("tags", function (Blueprint $table) {
+
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('name', 255);
+            $table->integer('profit_type'); //損(-1)益(1)
+            $table->string('comment', 255);
+            $table->timestamps();
+            $table->softDeletes();
+
+
+            //*********************************
+            // Foreign KEY [ Uncomment if you want to use!! ]
+            //*********************************
+            $table->foreign("user_id")->references("id")->on("users");
+
+
+            // ----------------------------------------------------
+            // -- SELECT [tags]--
+            // ----------------------------------------------------
+            // $query = DB::table("tags")
+            // ->leftJoin("users","users.id", "=", "tags.user_id")
+            // ->get();
+            // dd($query); //For checking
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists("tags");
+    }
+}
