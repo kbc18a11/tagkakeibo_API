442311c72b0d29b9c66d1fedab0f91bf5691a830
Naoumi1214 <wada121400@gmail.com>
2020-04-11 20:30:45 +0900

    ユーザテーブルに指定されたidの存在確認をするバリデーションルールを追加

--- app/Tag.php
+++ app/Tag.php
@@ -11,11 +11,20 @@ class Tag extends Model
     protected $table = 'Tags';
 
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id','name', 'profit_type', 'comment'
+    ];
+
+    /**
      * 初期登録のバリデーションの条件
      * @var array
      */
     private static $createrules = [
-        'user_id' => ['required', 'integer'],
+        'user_id' => ['required', 'integer','exists:users,id'],
         'name' => ['required', 'string', 'max:255'],
         'profit_type' => ['required', 'integer','between:-1,1'],
     ];
