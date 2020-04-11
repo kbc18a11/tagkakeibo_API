4ee48e91e6dcd41c12eea4d75681f708516e3d53
Naoumi1214 <wada121400@gmail.com>
2020-04-11 21:22:20 +0900

    タグの更新処理(update)を実装した

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -68,12 +68,43 @@ class TagController extends Controller
      * Update the specified resource in storage.
      *
      * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
+     * @param int $id
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(Request $request)
+    public function update(Request $request, int $id)
     {
-        //
+        $param = [
+            'updateResult' => true,
+        ];
+        //バリデーションの検証
+        $validationResult = Tag::updatevalidator($request->all());
 
+        //バリデーションの結果が駄目か？
+        if ($validationResult->fails()) {
+            # code...
+            $param['updateResult'] = false;
+            $param['error'] = $validationResult->messages();
+            return response()->json($param);
+        }
+
+        $tag = Tag::find($id);
+        //指定したidのタグは存在するか？ && 指定したタグのuser_idとユーザーのidが一致しないか？
+        if ($tag && !$tag->user_idCheck(auth()->id())) {
+            $param['updateResult'] = false;
+            $param['error'] = '更新できないタグを更新しようとしています';
+            return response()->json($param);
+        }
+
+        //タグ更新
+        $updateparam = [
+            'user_id' => auth()->id(),
+            'name' => $request->name,
+            'profit_type' => $request->profit_type,
+            'comment' => $request->comment
+        ];
+        $tag->fill($updateparam)->save();
+
+        return response()->json($param);
     }
 
     /**
