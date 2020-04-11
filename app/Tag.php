333c0d55ce0d5cac6bb1bcea7590b6060160f2ac
Naoumi1214 <wada121400@gmail.com>
2020-04-11 20:03:41 +0900

    新規データ作成用のバリデーションルールを実装

--- app/Tag.php
+++ app/Tag.php
@@ -3,9 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
 
 class Tag extends Model
 {
     //
     protected $table = 'Tags';
+
+    /**
+     * 初期登録のバリデーションの条件
+     * @var array
+     */
+    private static $createrules = [
+        'user_id' => ['required', 'integer'],
+        'name' => ['required', 'string', 'max:255'],
+        'profit_type' => ['required', 'integer','between:-1,1'],
+    ];
+
+    /**
+     * 初期登録のバリデーションの検証
+     * @param array
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    public static function createvalidator(array $array)
+    {
+        # code...
+        return Validator::make($array, Tag::$createrules);
+    }
 }
