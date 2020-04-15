a205893302ec462bad8f770c269c08eae6ce5e2a
Naoumi1214 <wada121400@gmail.com>
2020-04-15 17:37:00 +0900

    タグのいいねボタンのマイグレーションファイルを実装

---
+++ database/migrations/2020_04_15_082036_create_goodTags_table.php
@@ -0,0 +1,50 @@
+<?php
+            use Illuminate\Support\Facades\Schema;
+            use Illuminate\Database\Schema\Blueprint;
+            use Illuminate\Database\Migrations\Migration;
+
+            class CreateGoodTagsTable extends Migration
+            {
+                /**
+                 * Run the migrations.
+                 *
+                 * @return void
+                 */
+                public function up()
+                {
+                    Schema::create("goodTags", function (Blueprint $table) {
+						$table->increments('id');
+						$table->bigInteger('tag_id')->unsigned();
+						$table->bigInteger('user_id')->unsigned();
+						$table->timestamps();
+						$table->softDeletes();
+
+						$table->foreign("tag_id")->references("id")->on("tags");
+						$table->foreign("user_id")->references("id")->on("users");
+
+
+
+						// ----------------------------------------------------
+						// -- SELECT [goodTags]--
+						// ----------------------------------------------------
+						// $query = DB::table("goodTags")
+						// ->leftJoin("tag","tag.id", "=", "goodTags.tag_id")
+						// ->leftJoin("users","users.id", "=", "goodTags.user_id")
+						// ->get();
+						// dd($query); //For checking
+
+
+
+                    });
+                }
+
+                /**
+                 * Reverse the migrations.
+                 *
+                 * @return void
+                 */
+                public function down()
+                {
+                    Schema::dropIfExists("goodTags");
+                }
+            }
