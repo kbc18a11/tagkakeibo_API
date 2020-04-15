1e61601ef5c10433de08a3884392fa621462eb2e
Naoumi1214 <wada121400@gmail.com>
2020-04-15 18:43:07 +0900

    タグのいいねの新規登録の実装が完了した

---
+++ app/Http/Controllers/GoodTagController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tag;
+use Illuminate\Http\Request;
+use App\GoodTag;
+
+class GoodTagController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $param = [
+            'createResult' => true,
+        ];
+        //バリデーションの検証
+        $validationResult = GoodTag::createvalidator($request->all());
+
+        //バリデーションの結果が駄目か？
+        if ($validationResult->fails()) {
+            # code...
+            $param['createResult'] = false;
+            $param['error'] = $validationResult->messages();
+            return response()->json($param);
+        }
+
+        //いいね！登録
+        $createparam = [
+            'user_id' => auth()->id(),
+            'tag_id' => $request->tag_id
+        ];
+        GoodTag::create($createparam);
+
+        return response()->json($param);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
